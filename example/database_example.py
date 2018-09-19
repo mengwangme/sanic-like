ce12b1ef65becdd745d636bd802a74d237e1e33b
KeepitReal555 <faizwang@163.com>
2018-09-19 19:14:39 +0800

    async database

--- example/database_example.py
+++ example/database_example.py
@@ -1,23 +1,33 @@
-from peewee import *
-from datetime import date
+import asyncio
 
-db = MySQLDatabase('sanic_db', user='root', password='', host='127.0.0.1', port=3306)
+import peewee
+import peewee_async
+
+db = peewee_async.MySQLDatabase('sanic_testdb', user='root',
+                                password='', host='127.0.0.1',
+                                port=3306)
 
 # 定义表
-class Person(Model):
-    name = CharField()
-    birthday = DateField()
+class Person(peewee.Model):
+    name = peewee.CharField()
 
     class Meta:
-        database = db
+        database = db   # 指定数据库
+
+objects = peewee_async.Manager(db)
+
+Person.create_table(True)
+
+async def handler():
+    await objects.create(Person, name='Bob')
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(handler())
 
 
-if __name__ == '__main__':
 
-    # 连接数据库
-    db.connect()
-    # 创建表
-    db.create_tables([Person])
-    uncle_bob = Person(name='Bob', birthday=date(1960, 1, 15))
-    uncle_bob.save()
+
+
+
+
 
