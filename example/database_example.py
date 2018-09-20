a927acfd9dcd004989872f98cd6f42529962dccd
KeepitReal555 <faizwang@163.com>
2018-09-20 14:59:18 +0800

    fixed async database example

--- example/database_example.py
+++ example/database_example.py
@@ -1,7 +1,9 @@
+import sys
 import asyncio
 
 import peewee
 import peewee_async
+
 
 db = peewee_async.MySQLDatabase('sanic_testdb', user='root',
                                 password='', host='127.0.0.1',
@@ -23,6 +25,11 @@ async def handler():
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(handler())
+loop.close()
+if loop.is_closed():
+    sys.exit(0)
+
+
 
 
 
